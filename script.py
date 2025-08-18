a911adbad051112425c0dced7ed2624fdfa1e46e
Art3mis <pyatt.eve@gmail.com>
2025-08-18 21:41:52 +0000

    starting

---
+++ script.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.hashers import make_password
+hashed = make_password('mypw')
+
+data = json.loads(request.body)
+
+users = {}
+users['alice'] = 'pass123'
+
+# in /signup
+users[username] = make_password(password)
+
+# how to check username login password thingies
+check_password(password, users[username])
+
+request.session['user_id'] = user.user_id
+
+if requset.session.get('user_id'):
+    return render(request, 'profile.html')
+else:
+    return redirect('/login')
+
+# block access to private pages
+if not request.session.get('user_id'):
+    return redirect('/login')
+
+# logging out
+request.session.flush()
