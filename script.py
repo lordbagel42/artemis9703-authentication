c1eb163fab78dcc2c598e622d86668f830ff9406
Art3mis <pyatt.eve@gmail.com>
2025-08-18 23:02:51 +0000

    mooooooooooooooon

--- script.py
+++ script.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.hashers import make_password
 hashed = make_password('mypw')
 
+from django.contrib.auth.decorators import login_required
+@login_required
+def profile(request):
+    ...
+# finish defining function
+
 data = json.loads(request.body)
 
 users = {}
@@ -14,7 +20,7 @@ check_password(password, users[username])
 
 request.session['user_id'] = user.user_id
 
-if requset.session.get('user_id'):
+if request.session.get('user_id'):
     return render(request, 'profile.html')
 else:
     return redirect('/login')
@@ -25,3 +31,6 @@ if not request.session.get('user_id'):
 
 # logging out
 request.session.flush()
+
+#cookies yaaaaaaaaaaaaaaaaaaaaaay
+request.COOKIES.get('sessionid')
