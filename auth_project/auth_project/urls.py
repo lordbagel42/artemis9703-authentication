bc432fc1026614bc29339fe1abb165f09cbdd503
Art3mis <pyatt.eve@gmail.com>
2025-08-24 19:44:24 +0000

    sigh

--- auth_project/auth_project/urls.py
+++ auth_project/auth_project/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('users.urls')),
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
